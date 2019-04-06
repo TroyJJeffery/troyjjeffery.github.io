bb1367f242a6db2ebac56f8dc1da648419ca246e
TroyJJeffery <troyjjeffery@gmail.com>
2019-04-05 20:26:30 -0700

    4-5-19
    - Added some resources for my other classes to their respective folders.
    
    - Spent some time working on Chapter 5 in CS

--- Computer Science/CS 162/Chapter 5/chapter05/animal-monitoring-v1/AnimalMonitor.java
+++ Computer Science/CS 162/Chapter 5/chapter05/animal-monitoring-v1/AnimalMonitor.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.HashMap;
 
 /**
  * Monitor counts of different types of animal.
@@ -56,27 +57,52 @@ public class AnimalMonitor
      */
     public void printSightingsBy(int spotter)
     {
-        for(Sighting record : sightings) {
-            if(record.getSpotter() == spotter) {
-                System.out.println(record.getDetails());
-            }
-        }        
+        sightings.stream()
+                 .filter(s -> spotter == s.getSpotter())
+                 .map(s -> s.getDetails())
+                 .forEach(System.out::println);            
     }
     
     /**
+     * Creats an ArrayList of all the animals that have been monitored.
+     */
+    public ArrayList<String> animalList()
+    {
+        ArrayList<String> animals = new ArrayList<>();
+        
+        sightings.stream()
+                 .map(s -> s.getAnimal())
+                 .forEach(s -> animals.add(s));
+                 
+        return animals;
+                 
+    }
+
+    /**
      * Print a list of the types of animal considered to be endangered.
-     * @param animalNames A list of animals names.
      * @param dangerThreshold Counts less-than or equal-to to this level
      *                        are considered to be dangerous.
      */
-    public void printEndangered(ArrayList<String> animalNames, 
-                                int dangerThreshold)
+    public void printEndangered(int dangerThreshold)
     {
-        for(String animal : animalNames) {
-            if(getCount(animal) <= dangerThreshold) {
-                System.out.println(animal + " is endangered.");
-            }
-        }
+        HashMap<String,Integer> hashMap = new HashMap<>();  // Declares a hashMap for later.
+
+        ArrayList<String> animals = animalList(); // Calls a method above that puts all
+                                                  //animal names into an arrayList.
+        
+        for(String animal : animals){ //Stream to get total sightings for each animal.
+            int x = 0;// Declares x and resets it each loop.
+            x = sightings.stream()
+                         .filter(s -> animal.equals(s.getAnimal()))// Filter by animal.
+                         .map(s -> s.getCount())// Pull out count values.
+                         .reduce(0, (a, b) -> a+b); // Reduce to a single int with a sum.
+                         
+            if(x<=dangerThreshold){// If the total for the animal is less than the
+                                   //Threshold it adds it to the map.
+                hashMap.put(animal, x);
+                    }
+                }
+        System.out.println(hashMap.entrySet()); // Prints all entrys in the map.
     }
     
     /**
@@ -171,4 +197,16 @@ public class AnimalMonitor
                  .forEach(s -> System.out.println(s.getDetails()));
     }
     
+    /**
+     * Prints the total count of the sightings of one animal.
+     * @param animal The animal that you would like the count for.
+     */
+    public void printTotalSightingsOf(String animal)
+    {
+        sightings.stream()
+                 .filter(s -> animal.equals(s.getAnimal()))
+                 .map(s -> s.getCount())
+                 .forEach(System.out::println);
+                 
+    }
     }
