606fc32261873367d3c561bef35409e9ab98eb36
TroyJJeffery <43617703+TroyJJeffery@users.noreply.github.com>
2019-10-03 16:24:00 -0700

    finished fraction

---
+++ Computer Science/Data Structures/FractionModule.py
@@ -0,0 +1,106 @@
+#Finds the least common denominator between two fractions.
+def gcd(m,n):
+
+    while m%n != 0:
+        oldm=m
+        oldn=n
+
+
+        m=oldn
+        n=oldm%oldn
+    return n
+
+class Fraction:
+    #Constructor for class Fraction
+    def __init__(self, top, bottom):
+        if top <0 and bottom <0: # if -/- result +/+
+            bottom*-1
+
+        if top > 0 and bottom < 0: # if +/- result -/+
+            top = top*-1
+            bottom = bottom*-1
+
+        if top == 0: # if 0/x result 0/0
+            top = 0
+            bottom = 0
+
+        if bottom == 0: # if x/0 result error
+            raise ValueError ("Cannot divide by zero.")
+        
+        cd = gcd(top,bottom)
+
+        self.num = top//cd
+        self.den = bottom//cd
+
+    #Converts fraction into a string.
+    def __str__(self):
+
+        return str(self.num)+"/"+str(self.den)
+
+    #Adds two fractions together.
+    def __add__(self, otherfraction):
+
+        newnum = self.num*otherfraction.den+self.den*otherfraction.num
+        newden = self.den*otherfraction.den
+        common = gcd(newnum,newden)
+
+        return Fraction(newnum//common,newden//common)
+
+    #Subtracts one fraction from another.
+    def __sub__(self, frac):
+        # a/b-c/d
+        ad = self.num * frac.den
+        bc = self.den * frac.num
+        newden = self.den * frac.den
+        newnum = ad - bc
+        common = gcd(newnum, newden)
+
+        return Fraction(newnum // common, newden // common)
+
+    #Multiplies two fractions.
+    def __mul__(self, frac):
+        newnum = self.num * frac.num
+        newden = self.den * frac.den
+        cd = gcd(newnum, newden)
+
+        return Fraction(newnum // cd, newden // cd)
+
+    #Divides one fraction into another.
+    def __truediv__(self, frac):
+        newnum = self.num * frac.den
+        newden = self.den * frac.num
+
+        return Fraction(newnum, newden)
+
+    #Compares two fractions to each other.
+    def __eq__(self, other):
+        firstnum = self.num * other.den
+        secondnum = other.num * self.den
+
+        return firstnum == secondnum
+
+    #Checks if two fractions are not equal.
+    def __ne__(self, other):
+        return not __eq__(self,other)
+
+    #Checks if one fraction is greater than another.
+    def __gt__(self, frac):
+        firstnum = self.num * frac.den
+        secondnum = self.den * frac.num
+
+        return firstnum > secondnum
+
+    #Checks to see if one fraction is less than another.
+    def __lt__(self, frac):
+
+        return not self.__gt__(frac)
+
+    #Formats fractions for display.
+    def show(self):
+
+        print(self.num, "/", self.den)
+
+
+############################################################################################
+if __name__ == '__main__':
+    pass
---
+++ Computer Science/Data Structures/Unit Testing.py
@@ -0,0 +1,103 @@
+import unittest
+import FractionModule
+
+class FractionTest(unittest.TestCase):
+
+    #Verifies addition functions are working normally.
+    def test_add(self):
+        f1 = FractionModule.Fraction(1,2)
+        f2 = FractionModule.Fraction(1,4)
+        f3 = FractionModule.Fraction(-1,2)
+        f4 = FractionModule.Fraction(1,-4)
+        f5 = FractionModule.Fraction(-1,-2)
+
+        self.assertEqual(f1+f2, FractionModule.Fraction(3,4))
+        self.assertEqual(f3+f4, FractionModule.Fraction(-3,4))
+        self.assertEqual(f5+f5, FractionModule.Fraction(1,1))
+
+    #Verifies subtracting functions are working normally.
+    def test_subtract(self):
+        f1 = FractionModule.Fraction(1,2)
+        f2 = FractionModule.Fraction(1,4)
+        f3 = FractionModule.Fraction(-1,2) #-2/4
+        f4 = FractionModule.Fraction(1,-4) #-1/4
+        f5 = FractionModule.Fraction(-1,-2) #1/2
+
+        self.assertEqual(f1-f2, FractionModule.Fraction(1,4))
+        self.assertEqual(f3-f4, FractionModule.Fraction(-1,4))
+        #self.assertRaises(ValueError, f5-f5, FractionModule.Fraction(2,2))
+
+    #Verifies multiplication functionality.
+    def test_multiply(self):
+        f1 = FractionModule.Fraction(1,2)
+        f2 = FractionModule.Fraction(1,4)
+        f3 = FractionModule.Fraction(-1,2)
+        f4 = FractionModule.Fraction(1,-4)
+        f5 = FractionModule.Fraction(-1,-2)
+
+        self.assertEqual(f1*f2, FractionModule.Fraction(1,8))
+        self.assertEqual(f3*f4, FractionModule.Fraction(1,8))
+        self.assertEqual(f5*f5, FractionModule.Fraction(1,4))
+
+    #verifies division capabilities.
+    def test_divide(self):
+        f1 = FractionModule.Fraction(1,2)
+        f2 = FractionModule.Fraction(1,4)
+        f3 = FractionModule.Fraction(-1,2)
+        f4 = FractionModule.Fraction(1,-4)
+        f5 = FractionModule.Fraction(-1,-2)
+
+        self.assertEqual(f1/f2, FractionModule.Fraction(2,1))
+        self.assertEqual(f3/f4, FractionModule.Fraction(2,1))
+        self.assertEqual(f5/f5, FractionModule.Fraction(1,1))
+
+    #Verifies fraction equality is working correctly.
+    def test_equals(self):
+        f1 = FractionModule.Fraction(1,2)
+        f2 = FractionModule.Fraction(1,4)
+        f3 = FractionModule.Fraction(-1,2)
+        f4 = FractionModule.Fraction(1,-4)
+        f5 = FractionModule.Fraction(-1,-2)
+
+        self.assertEqual(f1==f2, False)
+        self.assertEqual(f3==f4, False)
+        self.assertEqual(f5==f5, True)
+
+    #Verifies that function greater than equality.
+    def test_greaterthan(self):
+        f1 = FractionModule.Fraction(1,2)
+        f2 = FractionModule.Fraction(1,4)
+        f3 = FractionModule.Fraction(-1,2)
+        f4 = FractionModule.Fraction(1,-4)
+        f5 = FractionModule.Fraction(-1,-2)
+
+        self.assertEqual(f1>f2, True)
+        self.assertEqual(f3>f4, False)
+        self.assertEqual(f5>f5, False)
+
+    #verifies that lessser than functions work.
+    def test_lessthan(self):
+        f1 = FractionModule.Fraction(1,2)
+        f2 = FractionModule.Fraction(1,4)
+        f3 = FractionModule.Fraction(-1,2)
+        f4 = FractionModule.Fraction(1,-4)
+        f5 = FractionModule.Fraction(-1,-2)
+
+        self.assertEqual(f1<f2, False)
+        self.assertEqual(f3<f4, True)
+        self.assertEqual(f5<f5, True)
+
+    #Verifies that strings are being created correctly.
+    def test_string(self):
+        f1 = FractionModule.Fraction(1,2)
+        f2 = FractionModule.Fraction(1,4)
+        f3 = FractionModule.Fraction(-1,2)
+        f4 = FractionModule.Fraction(1,-4)
+        f5 = FractionModule.Fraction(-1,-2)
+
+        self.assertEqual(f1==f2, False)
+        self.assertEqual(f3==f4, False)
+        self.assertEqual(f5==f5, True)
+
+if __name__ == '__main__':
+    unittest.main(exit=False)
